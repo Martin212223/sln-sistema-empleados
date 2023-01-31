3c01d3f7efc9ee6b8b17ad2e8f3891308d8a12f2
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-31 18:10:01 -0300

    haciendo la validación de fecha

---
+++ SistemaWebEmpleado/Models/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaWebEmpleado.Models
+{
+    [Table("Empleado")]
+    public class Empleado
+    {
+
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Apellido { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string DNI { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        [RegularExpression("[A]{2}[1-9]{5}$")]
+        //[RegularExpression("@([A]{2})+([1-9]{5})")]
+        public string Legajo { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Titulo { get; set; }
+
+    }
+}
---
+++ SistemaWebEmpleado/Validations/AfterYear2000Attribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWebEmpleado.Validations
+{
+    public class AfterYear2000Attribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            
+            DateTime fecha = Convert.ToDateTime(value);
+
+            if (fecha < new DateTime())
+
+        }
+
+    }
+}
